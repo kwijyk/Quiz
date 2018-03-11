ab33b24e4ddb882f90e12cad23a4507bd2ce7859
Sergey <ser.olegov@gmail.com>
2018-03-11 23:42:36 +0200

    added for life mode

---
+++ Quiz/Controllers/Levels/LevelsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LevelsViewController.swift
+//  Quiz
+//
+//  Created by Sergey Gaponov on 3/11/18.
+//  Copyright © 2018 Sergio. All rights reserved.
+//
+
+import UIKit
+
+class PlayLevelsViewController: UIViewController {
+
+    enum GameModeType {
+        case forLife
+        case forTime
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func firstLevelDidSelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func secondLevelDidSelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func thirdLevelDidSelected(_ sender: UIButton) {
+    }
+}
